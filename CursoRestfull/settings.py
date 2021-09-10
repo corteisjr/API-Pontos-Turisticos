f81d2a1267dc566f526e679f73728e56ef89070a
corteisjr <corteisjunior@gmail.com>
2021-09-10 07:56:23 +0200

    add app comentarios

--- CursoRestfull/settings.py
+++ CursoRestfull/settings.py
@@ -38,7 +38,9 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'pontos_turisticos',
-    'atracoes'
+    'atracoes',
+    'comentarios',
+    'avaliacoes',
 ]
 
 MIDDLEWARE = [
---
+++ comentarios/admin.py
@@ -0,0 +1,3 @@
+from django.contrib import admin
+from .models import Comentario
+admin.site.register(Comentario)

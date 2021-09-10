e9058d25a8313776eade0496b25fc97d7a24fd58
corteisjr <corteisjunior@gmail.com>
2021-09-10 08:49:16 +0200

    Finalizando app models

--- avaliacoes/models.py
+++ avaliacoes/models.py
@@ -5,11 +5,11 @@ from django.db import models
 class Avaliacao(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE)
     comentario = models.TextField(null=True, blank=True)
-    nota = models.DecimalField()
-    data = models.DataTimeField(auto_now_add=True)
+    nota = models.DecimalField(max_digits=3, decimal_places=2)
+    data = models.DateTimeField(auto_now_add=True)
 
 
     def __str__(self):
-        return user.first_name
+        return self.user
 
 
--- pontos_turisticos/models.py
+++ pontos_turisticos/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 from atracoes.models import Atracao
 from comentarios.models import Comentario
+from avaliacoes.models import Avaliacao
 
 
 class PontoTuristico(models.Model):
@@ -9,6 +10,7 @@ class PontoTuristico(models.Model):
     aprovado = models.BooleanField(default=False)
     atracoes = models.ManyToManyField(Atracao)
     comentarios = models.ManyToManyField(Comentario)
+    avaliacoes = models.ManyToManyField(Avaliacao)
 
     def __str__(self):
         return self.nome

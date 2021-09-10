c8b0dc1b27d778776c07e49f16900ad66b2c3637
corteisjr <corteisjunior@gmail.com>
2021-09-10 07:40:49 +0200

    first commit

---
+++ pontos_turisticos/models.py
@@ -0,0 +1,12 @@
+from django.db import models
+from atracoes.models import Atracao
+
+
+class PontoTuristico(models.Model):
+    nome = models.CharField(max_length=150)
+    descricao = models.TextField()
+    aprovado = models.BooleanField(default=False)
+    atracoes = models.ManyToManyField(Atracao)
+
+    def __str__(self):
+        return self.nome

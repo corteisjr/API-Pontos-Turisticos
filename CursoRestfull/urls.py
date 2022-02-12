4205c17c2b9196e99e519e89c15dc912955f3b0f
corteisjr <corteisjunior@gmail.com>
2022-02-12 10:18:48 +0200

    finish with all end-point apps

--- CursoRestfull/urls.py
+++ CursoRestfull/urls.py
@@ -6,12 +6,16 @@ from pontos_turisticos.api.viewsets import PontoTuristicoViewSet
 from atracoes.api.viewset import AtracoesViewSet
 from endereco.api.viewsets import EnderecosViewSet
 from comentarios.api.viewsets import ComentariosViewSet
+from avaliacoes.api.viewsets import AvaliacoesViewSet
+
 
 router = routers.DefaultRouter()
 router.register(r'pontoturisticos', PontoTuristicoViewSet)
 router.register(r'atracoes', AtracoesViewSet)
 router.register(r'enderecos', EnderecosViewSet)
 router.register(r'comentarios', ComentariosViewSet)
+router.register(r'avaliacoes', AvaliacoesViewSet)
+
 
 urlpatterns = [
     path('', include(router.urls)),

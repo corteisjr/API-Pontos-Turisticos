74af144c42ffd676ba1f89c3c09930673d6d4302
corteisjr <corteisjunior@gmail.com>
2022-02-19 20:39:46 +0200

    Add TokenAuthentication

--- pontos_turisticos/api/viewsets.py
+++ pontos_turisticos/api/viewsets.py
@@ -1,11 +1,14 @@
 from rest_framework.response import Response
 from rest_framework.decorators import action
 from rest_framework.viewsets import ModelViewSet
+from rest_framework.permissions import IsAuthenticated
+from rest_framework.authentication import TokenAuthentication
 from pontos_turisticos.models import PontoTuristico
 from .serializers import PontoTuristicoSerializer
 
 class PontoTuristicoViewSet(ModelViewSet):
-
+    permission_classes = [IsAuthenticated,]
+    authentication_classes = (TokenAuthentication, )
     serializer_class = PontoTuristicoSerializer
     lookup_field = 'nome'
     def get_queryset(self):
@@ -39,8 +42,8 @@ class PontoTuristicoViewSet(ModelViewSet):
     # Sobscrevendo  a action de GET
 
 
-    def list(self, request, *args, **kwargs):
-        return Response({'Teste': 123})
+    # def list(self, request, *args, **kwargs):
+    #     return Response({'Teste': 123})
     
     #Sobscrevendo a action POST
     def create(self, request, *args, **kwargs):

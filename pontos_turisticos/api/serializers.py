e642a29af0be569085e54db6ca5b7db18d84a031
corteisjr <corteisjunior@gmail.com>
2022-02-19 21:01:00 +0200

    know about nested serializer

--- pontos_turisticos/api/serializers.py
+++ pontos_turisticos/api/serializers.py
@@ -1,8 +1,16 @@
 from rest_framework.serializers import ModelSerializer
 from pontos_turisticos.models import PontoTuristico
+from atracoes.api.serializers import AtracaoSerializer
+from endereco.api.seriealizers import EnderecosSerializer
+from comentarios.api.seriealizers import ComentarioSerializer
+from avaliacoes.api.seriealizers import AvaliacaoSerializer
 
 
 class PontoTuristicoSerializer(ModelSerializer):
+    atracoes = AtracaoSerializer(many=True)
+    enderecos = EnderecosSerializer()
+    comentarios = ComentarioSerializer(many=True)
+    
     class Meta:
         model = PontoTuristico
-        fields = ('id', 'nome', 'descricao', 'aprovado', 'foto')
+        fields = ('id', 'nome', 'descricao', 'aprovado', 'foto', 'atracoes', 'comentarios', 'avaliacoes', 'enderecos')

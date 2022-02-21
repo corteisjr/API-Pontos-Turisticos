ec6bc567db392d0c0d708708c2376379b4f9f8ce
corteisjr <corteisjunior@gmail.com>
2022-02-21 12:27:15 +0200

    Deploy

--- CursoRestfull/settings.py
+++ CursoRestfull/settings.py
@@ -128,6 +128,9 @@ STATIC_URL = '/static/'
 MEDIA_ROOT = 'imagens'
 MEDIA_URL = '/media/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATICFILES_STORAGE='whitenoise.django.GzipManifestStaticFilesStorage'
+
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
